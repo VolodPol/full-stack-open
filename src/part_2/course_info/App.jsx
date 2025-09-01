80f99ab1d0d373c810ba52eba9b7a25ee0c93eb6
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2025-09-01 20:04:28 +0300

    update: 2.1-2.4 course info

---
+++ src/part_2/course_info/App.jsx
@@ -0,0 +1,95 @@
+
+const Part = ( { name, numberOfExercises } ) => {
+    return (
+        <p>
+            {name} {numberOfExercises}
+        </p>
+    );
+}
+
+const Header = ( { name} ) => {
+    return (
+        <h2>{name}</h2>
+    );
+}
+
+const Content = ( { contents } ) => {
+    let totalCount = contents.map(p => p.exercises).reduce((sum, current) => sum + current, 0);
+    return (
+        <>
+            {
+                contents.map(
+                    (part) => <Part key={part.id} name={part.name} numberOfExercises={part.exercises}/>
+                )
+            }
+            <p><b>total of {totalCount} exercises</b></p>
+        </>
+    );
+}
+
+const Course = ({ header, parts }) => {
+    return (
+        <div>
+            <Header name={header}/>
+            <Content contents={parts}/>
+        </div>
+    );
+}
+
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Web development curriculum</h1>
+            {
+                courses.map(course => <Course key={course.id} header={course.name} parts={course.parts}/>)
+            }
+        </div>
+);
+}
+
+export default App
