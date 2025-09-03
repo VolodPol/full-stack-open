de2b6d3b48aeb43ac20bab5054777bef892b5e9f
Vova Polishchuk <volodimirpolishchuck@gmail.com>
2025-09-03 20:12:54 +0300

    update: 2.6 phonebook

---
+++ src/phonebook/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ]);
+    const [newName, setNewName] = useState('');
+
+
+    const updateNewName = (event) => {
+        setNewName(event.target.value);
+    }
+
+    const submitPerson = (event) => {
+        event.preventDefault();
+        let person = { name: newName };
+        setPersons(persons.concat(person));
+        setNewName('');
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={submitPerson}>
+                <div>
+                    name: <input value={newName} onChange={updateNewName} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {
+                persons.map(p =>
+                    <p key={p.name}>{p.name}</p>
+                )
+            }
+        </div>
+    )
+}
+
+export default App
